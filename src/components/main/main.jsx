a3af74a19c88fc543f0595976ce70f17ceb12e28
randomrationalizer <lifelower@gmail.com>
2020-10-02 20:58:52 +0300

    Удаляет закомментированный фрагмент кода

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -86,12 +86,6 @@ const Main = (props) => {
                   <li className="places__option" tabIndex="0">Price: high to low</li>
                   <li className="places__option" tabIndex="0">Top rated first</li>
                 </ul>
-                {/* <select className="places__sorting-type" id="places-sorting" defaultValue="popular">
-                  <option className="places__option" value="popular">Popular</option>
-                  <option className="places__option" value="to-high">Price: low to high</option>
-                  <option className="places__option" value="to-low">Price: high to low</option>
-                  <option className="places__option" value="top-rated">Top rated first</option>
-                </select> */}
               </form>
               <div className="cities__places-list places__list tabs__content">
                 <article className="cities__place-card place-card">
