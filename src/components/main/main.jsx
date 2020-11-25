ceb273a1ad661f4719a7751a073d9fd771268627
randomrationalizer <lifelower@gmail.com>
2020-11-25 15:46:27 +0300

    Добавляет компонент для отображения на главной при пустом списке офферов

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,18 +3,15 @@ import {connect} from "react-redux";
 import PropTypes from "prop-types";
 import {offersPropTypes} from "../offer/offer.prop";
 import {cityPropTypes} from "../cities/city.prop";
-import Map from "../map/map";
-import OffersList from "../offer/offers-list/offers-list";
+import OffersContainer from "../offer/offers-container/offers-container";
+import OffersEmptyContainer from "../offer/offers-empty-container/offers-empty-container";
 import CitiesList from "../cities/cities-list/cities-list";
-import SortSection from "../sort/sort-section/sort-section";
-import {OfferType, MapType} from "../../const";
 import logo from "../../../public/img/logo.svg";
-import withActiveState from "../../hocs/with-active-state/with-active-state";
 
-const SortSectionWrapped = withActiveState(SortSection);
 
 const MainPage = (props) => {
   const {favorites, currentCity, cityOffers, activeItem, onActiveItemChange} = props;
+  const isAnyOffers = cityOffers.length ? true : false;
 
   return (
     <div className="page page--gray page--main">
@@ -41,7 +38,7 @@ const MainPage = (props) => {
         </div>
       </header>
 
-      <main className="page__main page__main--index">
+      <main className={`page__main page__main--index ${isAnyOffers ? `` : `page__main--index-empty`}`}>
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
@@ -50,30 +47,19 @@ const MainPage = (props) => {
             />
           </section>
         </div>
-        <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{cityOffers.length} places to stay in {currentCity.name}</b>
-              <SortSectionWrapped />
-              <div className="cities__places-list places__list tabs__content">
-                <OffersList
-                  offers={cityOffers}
-                  favorites={favorites}
-                  onOfferHover={onActiveItemChange}
-                  offerType={OfferType.MAIN}
-                />
-              </div>
-            </section>
-            <div className="cities__right-section">
-              <Map
-                activeCardId={activeItem}
-                mapType={MapType.MAIN}
-                city={currentCity}
-              />
-            </div>
-          </div>
-        </div>
+        {isAnyOffers ?
+          <OffersContainer
+            offers={cityOffers}
+            favorites={favorites}
+            onOfferHover={onActiveItemChange}
+            activeCardId={activeItem}
+            city={currentCity}
+          />
+          :
+          <OffersEmptyContainer
+            city={currentCity}
+          />
+        }
       </main>
     </div>
   );
